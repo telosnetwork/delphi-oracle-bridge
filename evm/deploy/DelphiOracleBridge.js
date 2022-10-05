6935e4076eaffd8d7fb7899d924269cb324dca98
tomtomcrypto <titiomm@gmail.com>
2022-10-05 16:14:59 +0000

    added max callback gas value to deploy

--- evm/deploy/DelphiOracleBridge.js
+++ evm/deploy/DelphiOracleBridge.js
@@ -5,7 +5,7 @@ module.exports = async ({getNamedAccounts, deployments}) => {
     console.log(deployer)
     const bridge = await deploy('DelphiOracleBridge', {
         from: deployer,
-        args: ["1000000000000000000", 10, "0x6c33bdd2622e59fd10b411ff8d8d8d4dc5caf6ce", "0x648ac5a8c4E1ae5A93cd5BeDF143B095B8c49a2a"],
+        args: ["1000000000000000000", 10, "900000000" "0x6c33bdd2622e59fd10b411ff8d8d8d4dc5caf6ce", "0x648ac5a8c4E1ae5A93cd5BeDF143B095B8c49a2a"],
     });
 
     console.log("Bridge deployed to:", bridge.address);
